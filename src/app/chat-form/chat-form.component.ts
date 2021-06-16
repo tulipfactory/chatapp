b016e4626308c8b98d9a8ae57091f4489b8a856d
Talulah <79481227+tulipfactory@users.noreply.github.com>
2021-06-16 01:32:32 -0500

    updateupdate

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../services/chat.service';
+
 
 @Component({
   selector: 'app-chat-form',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-form.component.css']
 })
 export class ChatFormComponent implements OnInit {
+message: string;
 
-  constructor() { }
+ 
+
+  constructor(private chat: ChatService) { }
 
   ngOnInit(): void {
   }
+send() {
+  this.chat.sendMessage(this.message);
+}
 
+handleSubmit(event) {
+  if (event.keyCode === 13) {
+  this.send();}
+  }
 }
